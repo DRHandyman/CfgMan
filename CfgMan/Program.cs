6bd6ca89b0bf26e319c0d513dac946bfd01605dd
DRHandyMan <alexandralibekov@yahoo.com>
2022-06-18 16:26:43 +0400

    Updated Program.cs

--- CfgMan/Program.cs
+++ CfgMan/Program.cs
@@ -1,12 +1,23 @@
-﻿namespace test
+﻿using CfgMan;
+
+namespace test
 {
     class Program
     {
         static void Main()
         {
-            CfgMan.CfgMan m_CfgMan = new("test.cfwg");
+            CfgManipulator m_CfgManipulator;
 
-            Console.WriteLine(m_CfgMan.read("string"));
+            if (CfgManipulator.FileExists("test.cfg"))
+            {
+                m_CfgManipulator = new("test.cfg");
+
+                if (m_CfgManipulator.Contains("section", CfgManipulator.Types.SECTION))
+                {
+                    if (m_CfgManipulator.Contains("section", "string"))
+                        Console.WriteLine(m_CfgManipulator.Read("section", "string"));
+                }
+            }
         }
     }
 }
